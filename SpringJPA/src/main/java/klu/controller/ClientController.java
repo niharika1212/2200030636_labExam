81f59e1e25d30287560aa91832ec256502e37b8a
niharika1212 <116939897+niharika1212@users.noreply.github.com>
2024-12-07 09:08:22 +0530

    Add files via upload

---
+++ SpringJPA/src/main/java/klu/controller/ClientController.java
@@ -0,0 +1,19 @@
+package klu.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping("/")
+
+public class ClientController {
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mvc=new ModelAndView();
+		mvc.setViewName("home.jsp");
+		return mvc;
+	}
+	
+	
+}
